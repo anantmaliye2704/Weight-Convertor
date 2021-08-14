275a14860f64b24585843da70e554226e04b89ef
Anant Maliye <65802411+anantmaliye2704@users.noreply.github.com>
2021-08-15 00:18:15 +0530

    Add files via upload

---
+++ weights.js
@@ -0,0 +1,17 @@
+
+
+  function weightConverterKG(valNum) {
+    document.getElementById("outputKGrams").innerHTML = valNum * 2.2046;
+  }
+
+  function weightConverterOunce(valNum) {
+    document.getElementById("outputOunce").innerHTML = valNum * 35.274;
+  }
+
+  function weightConverterGrams(valNum) {
+    document.getElementById("outputGrams").innerHTML = valNum * 1000;
+  }
+
+  function weightConverterStones(valNum) {
+    document.getElementById("outputStones").innerHTML = valNum * 0.1574;
+  }
